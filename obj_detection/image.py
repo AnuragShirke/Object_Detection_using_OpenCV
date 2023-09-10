4b9ead6cdec1cc87c98b81763c34fd0aa77b5c10
Anurag Shirke <103914368+AnuragShirke@users.noreply.github.com>
2023-09-10 11:35:04 +0530

    image.py

--- obj_detection/image.py
+++ obj_detection/image.py
@@ -1,19 +1,20 @@
 import cv2
+import matplotlib.pyplot as plt
 
 # Description:
-# This script demonstrates object detection using a pre-trained model on a video stream.
-# Note: Use relative paths for better code portability. Ensure the frozen model is available in the script folder.
+# This script demonstrates object detection using a pre-trained model on an image.
+# Note: Whenever possible, use relative paths for better code portability.
 
-# Define the paths to the configuration file and frozen model (use 'frozen_inference_graph.pb' from the folder).
-config_file = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-frozen_model = 'frozen_inference_graph.pb'
+# Define the paths to the configuration file and frozen model. Replace with your actual file paths.
+config_file = 'path_to_config_file.pbtxt'
+frozen_model = 'path_to_frozen_model.pb'
 
 # Load the pre-trained model and labels
 model = cv2.dnn.DetectionModel(frozen_model, config_file)
 
-# Load class labels from a text file (replace with your labels file).
+# Load class labels from a text file. Replace with your labels file path.
 classLabels = []
-file_name = 'labels.txt'
+file_name = 'path_to_labels.txt'
 with open(file_name, 'rt') as fpt:
     classLabels = fpt.read().rstrip('\n').split('\n')
 
@@ -23,30 +24,22 @@ model.setInputScale(1.0 / 127.5)
 model.setInputMean((127.5, 127.5, 127.5))
 model.setInputSwapRB(True)
 
-# Open the video file or use the default camera (camera index 0)
-cap = cv2.VideoCapture(0)  # Use the default camera (camera index 0)
+# Load and process the image. Replace with your image file path.
+img = cv2.imread('path_to_image.jpg')
 
-if not cap.isOpened():
-    raise IOError("Cannot open video")
+# Perform object detection on the image
+ClassIndex, confidence, bbox = model.detect(img, confThreshold=0.5)
 
+# Define font settings for labeling detected objects
 font_scale = 3
 font = cv2.FONT_HERSHEY_PLAIN
 
-while True:
-    ret, frame = cap.read()
+# Draw bounding boxes and labels on the image
+for ClassInd, conf, boxes in zip(ClassIndex.flatten(), confidence.flatten(), bbox):
+    cv2.rectangle(img, boxes, (255, 0, 0), 2)
+    cv2.putText(img, classLabels[ClassInd - 1], (boxes[0] + 10, boxes[1] + 40), font, fontScale=font_scale, color=(0, 255, 0), thickness=3)
 
-    ClassIndex, confidence, bbox = model.detect(frame, confThreshold=0.55)
+# Display the annotated image
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.show()
 
-    if len(ClassIndex):
-        for ClassInd, conf, boxes in zip(ClassIndex.flatten(), confidence.flatten(), bbox):
-            if ClassInd <= 80:  # Assuming you have 80 classes
-                cv2.rectangle(frame, boxes, (255, 0, 0), 2)
-                cv2.putText(frame, classLabels[ClassInd - 1], (boxes[0] + 10, boxes[1] + 40), font, fontScale=font_scale, color=(0, 255, 0), thickness=3)
-
-        cv2.imshow('Object Detection Tutorial', frame)
-
-    if cv2.waitKey(2) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
